6ef08364011bb3e44216442b1e8afd2a641e2d7f
yorkglez <bobo_12yors@hotmail.com>
2018-05-02 19:56:15 -0500

    Initial commit

---
+++ Assets/Scripts/ColorChange.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour {
+
+	// Use this for initialization
+	//public defaultColor: Material;
+	private Renderer render;
+	public Material[] material;
+	public GameObject[] objs;
+	int i = 0;
+	void Start () {
+		//render = GetComponent<Renderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Alpha1)) {
+			objs = GameObject.FindGameObjectsWithTag ("CubeL1");
+			i = 0;
+		}
+		else if (Input.GetKeyDown (KeyCode.Alpha2)) {
+			GetComponent <MeshRenderer> ().material = material [1];
+			objs = GameObject.FindGameObjectsWithTag ("CubeL2");
+			i = 1;
+		}
+		else if (Input.GetKeyDown (KeyCode.Alpha3)) {
+			objs = GameObject.FindGameObjectsWithTag ("CubeL3");
+			i = 2;
+		}
+			
+		/*foreach (GameObject obj in objs) {
+			obj.GetComponent <MeshRenderer> ().material = material[i];
+		}	*/	
+		
+	}
+}
---
+++ Assets/Scripts/MoveWall.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveWall : MonoBehaviour {
+	public Rigidbody wall;
+	public float dir = 1f, speed;
+	// Update is called once per frame
+	void Update () {
+		Vector3 rigispeed = wall.velocity;
+		rigispeed.z = speed * dir;
+		wall.velocity = rigispeed;
+	}
+	void OnCollisionEnter(Collision col){
+		dir = -dir;
+		if(col.gameObject.tag=="Player"){
+			Destroy(col.gameObject);
+
+		}
+	}
+}
